9cd9e20dc353a02bb5939992a1bbc7c23967f56f
sakineh <sakineh.amiri22@gmail.com>
2024-04-08 00:13:29 +0430

    login.js  blur event

--- login.js
+++ login.js
@@ -1,17 +1,41 @@
-let emailInput = document.getElementById("email");
-function charchecker1() {
-  if (emailInput.value.length < 12) {
-    alert("you should enter at least 12 charector for email.");
+// let emailInput = document.getElementById("email");
+// function charchecker1() {
+//   if (emailInput.value.length < 12) {
+//     alert("you should enter at least 12 charector for email.");
+//   } else {
+//     alert("email is good.");
+//   }
+// }
+
+// let passwordInput = document.getElementById("password");
+// function charchecker2() {
+//   if (passwordInput.value.length < 6) {
+//     alert("you should enter at least 6 charector for password.");
+//   } else {
+//     alert("password is good.");
+//   }
+// }
+
+////////////////////////////////////////////////////////////
+//sending massage after entering input by onblur Event in js//
+////////////////////////////////////////////////////////////
+//email
+let email = document.querySelector("#email");
+function emailBlur() {
+  if (email.value.length < 12) {
+    alert("email must contain at least 12 characters.");
   } else {
-    alert("email is good.");
+    alert("correct email value.");
   }
 }
 
-let passwordInput = document.getElementById("password");
-function charchecker2() {
-  if (passwordInput.value.length < 6) {
-    alert("you should enter at least 6 charector for password.");
+//password
+
+let password = document.querySelector("#password");
+function passwordBlur() {
+  if (password.value.length < 6) {
+    alert("password must contain at least 6 characters.");
   } else {
-    alert("password is good.");
+    alert("correct password value.");
   }
 }
